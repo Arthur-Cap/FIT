cf5a82f7e79a56af03d8df38a325b1c7c7d04f7a
Arthur-Cap <thanhdat.ctd2003@gmail.com>
2023-09-19 15:16:23 +0700

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./Components/Static/Header/header";
+import "./App.css";
+import SideBar from "./Components/Static/SideBar/SideBar";
+import React, { useState } from 'react';
 
 function App() {
+
+  const [isOpen, setOpen] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <SideBar isOpen={isOpen} setOpen={setOpen}></SideBar>
+      <Header isOpen={isOpen} setOpen={setOpen}></Header>
+      <page></page>
     </div>
   );
 }
