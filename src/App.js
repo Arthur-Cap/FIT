dd4b34f6ca780d7337464dce40a195a61e6038e5
Arthur-Cap <thanhdat.ctd2003@gmail.com>
2023-09-19 15:32:28 +0700

    Second cmt

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from "./Components/Static/Header/header";
 import "./App.css";
 import SideBar from "./Components/Static/SideBar/SideBar";
 import React, { useState } from 'react';
+import Page from "./Components/Static/Content/Page";
 
 function App() {
 
@@ -11,7 +12,7 @@ function App() {
     <div className="App">
 
       <SideBar isOpen={isOpen} setOpen={setOpen}></SideBar>
-      <Header isOpen={isOpen} setOpen={setOpen}></Header>
+      <Page  isOpen={isOpen} setOpen={setOpen}></Page>
       <page></page>
     </div>
   );
